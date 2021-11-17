647ab034f17330c506f08c84ebc0b2dbf9210de5
zhuting03 <zhuting03@baidu.com>
2021-11-17 17:00:57 +0800

    feat(error): 异常处理
    错误信息处理，抛出完整的信息

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,9 +1,18 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     return new Promise((resolve, reject) => {
-        const { url, method = 'get', data = null, params = null, headers, responseType } = config
+        const {
+            url,
+            method = 'get',
+            data = null,
+            params = null,
+            headers,
+            responseType,
+            timeout
+        } = config
         const request = new XMLHttpRequest()
         request.open(method.toUpperCase(), url, true)
 
@@ -16,8 +25,16 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
             }
         })
 
+        if (timeout) {
+            request.timeout = timeout
+        }
+
         request.onreadystatechange = () => {
             if (request.readyState !== 4) {
+                return
+            }
+
+            if (request.status === 0) {
                 return
             }
 
@@ -34,8 +51,40 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
                 request
             }
 
-            resolve(response)
+            handleResponse(response)
         }
+
+        request.onerror = () => {
+            reject(createError('Network Error', config, null, request))
+        }
+
+        request.ontimeout = () => {
+            reject(
+                createError(
+                    `Timeout of ${config.timeout} ms exceeded`,
+                    config,
+                    'ECONNABORTED',
+                    request
+                )
+            )
+        }
+
+        function handleResponse(response: AxiosResponse) {
+            if (response.status >= 200 && response.status < 300) {
+                resolve(response)
+            } else {
+                reject(
+                    createError(
+                        `Request failed with status code ${response.status}`,
+                        config,
+                        null,
+                        request,
+                        response
+                    )
+                )
+            }
+        }
+
         request.send(data)
     })
 }
