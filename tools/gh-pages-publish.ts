fd0e7010161b837ba38fee9b506cf7b161a89fcd
Echoting <870917335@qq.com>
2021-12-09 17:37:30 +0800

    fix: 安全检测

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -22,8 +22,8 @@ cd("docs")
 touch(".nojekyll")
 exec("git init")
 exec("git add .")
-exec('git config user.name "zhuting03"')
-exec('git config user.email "zhuting03@baidu.com"')
+exec('git config user.name "Echoting"')
+exec('git config user.email "870917335@qq.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
   `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
